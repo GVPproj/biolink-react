360446aceaaa2133d9410a0c9143db45c98cd4d9
GVPproj <69814261+GVPproj@users.noreply.github.com>
2022-12-19 10:31:48 -0800

    music vid nomenclature

--- src/links.js
+++ src/links.js
@@ -8,13 +8,13 @@ export default [
         mixCloud: "https://www.mixcloud.com/widget/iframe/?feed=%2Fgrahamvanpelt%2Fplaylists%2Fisland-city%2F"
     },
     {
-        linkText: "The Toll - Animation",
+        linkText: "'The Toll' - Animation",
         isYouTube: true,
         youTubeId: "gJaRNO18Y54",
         youTubeTitle: "The Toll - Video by Eli Horn"
     },
     {
-        linkText: "Geopotential - Animation",
+        linkText: "'Geopotential' - Animation",
         isYouTube: true,
         youTubeId: "xYTSCZTeGWE",
         youTubeTitle: "Geopotential - Video by Eli Horn"
